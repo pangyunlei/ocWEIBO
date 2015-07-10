a2c88281590ec644aba140ad3f756c246e5b3d8d
suzhaoyang <xiuzhaoyang2004@163.com>
2015-07-10 14:45:53 +0800

    多个

---
+++ 23/23/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  23
+//
+//  Created by LX on 15/7/10.
+//  Copyright (c) 2015年 like. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NEWTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NEWTableViewController.h
+//  01-网易新闻
+//
+//  Created by LX on 15/7/9.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NEWTableViewController : UITableViewController
+
+@end
